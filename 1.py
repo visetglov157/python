65b159f9cb487ea8940599f033e25756b4f74424
Vladislav <vladislav33lavrov@gmail.com>
2023-10-13 23:01:30 +0300

    1111

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
-number = True
-
-while number:
-    if input("enter your password: ")  ==  "123123":
-        number = False
-        print('WelCOme')
-    else:
-        print('The password is incorrect')
-
+# number = True
+#
+# while number:
+#     if input("enter your password: ")  ==  "123123":
+#         number = False
+#         print('WelCOme')
+#     else:
+#         print('The password is incorrect')
+#
