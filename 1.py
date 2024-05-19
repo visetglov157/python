664396e1c549108fdb2cf58bdd4c464c0a39a60f
Vladislav <115417107+visetglov157@users.noreply.github.com>
2024-05-19 16:44:05 +0300

    что-то

--- 1.py
+++
@@ -1,9 +0,0 @@
-# number = True
-#
-# while number:
-#     if input("enter your password: ")  ==  "123123":
-#         number = False
-#         print('WelCOme')
-#     else:
-#         print('The password is incorrect')
-#
---
+++ calc.py
@@ -0,0 +1,20 @@
+# a = int(input('enter first number: '))
+# action = int(input('''
+# 1 -- +
+# 2 -- -
+# 3 -- *
+# 4 -- /
+# Select an action: '''))
+# b = int(input('enter second number: '))
+# if action == 1:
+#     print(f'{a} + {b} = ',  a + b)
+# elif action == 2:
+#     print(f'{a} - {b} = ', a - b)
+# elif action == 3:
+#     print(f'{a} * {b} = ', a * b)
+# elif action == 4:
+#     print(f'{a} / {b} = ', a / b)
+# else:
+#     print('error')
+
+
