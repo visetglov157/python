5a7396620c9d1ae9b266c33dd013a33b945ecc6b
Vladislav <115417107+visetglov157@users.noreply.github.com>
2024-05-19 16:43:17 +0300

    что-то

--- lists.py
+++ lists.py
@@ -1,9 +1,12 @@
-password = True
-while password:
-    if input('введите ваш пароль: ') == "geg":
-        print('добро пожаловать!!!', end = ' ')
-        password = False
-    else:
-        print("ваш пароль неверный, попробуйте еще раз...", end= "")
+        # Выведите первый и последний элемент массива
+# array = [1, 6, 6, 1, 9, 16, 23]
+# print(f'''first elem: {array[0]}
+# last elem: {array[-1]}''')
+        # Выведите максимальный и минимальный элемент в массиве
+# list = [1, 6, 12, 85, 21, 4]
+# print(f'''max elem: {max(list)}
+# min elem: {min(list)}''')
+
+
 
 
